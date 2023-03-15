9037497a9dffccdeae91f72a40ef0f695dd881bb
Matheus Perdigão Assenheimer <127908612+MatheusPAssenheimer@users.noreply.github.com>
2023-03-15 19:12:36 -0300

    Add files via upload

---
+++ Atividade4/Atividade4/Triangulo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade4
+{
+    internal class Triangulo
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public double A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                a = value;
+            }
+        }
+
+        public double B
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                b = value;
+            }
+        }
+
+        public double C
+        {
+            get
+            {
+                return c;
+            }
+            set
+            {
+                c = value;
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
